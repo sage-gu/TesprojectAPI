9b40fd0cc48d7ed71f83be6a3aa26ff2f301567d
Sage Gu <sage.gu@ihealthlabs.com>
2021-01-13 11:09:06 +0800

    add coverage step

--- test_class.py
+++ test_class.py
@@ -1,9 +1,14 @@
 # content of test_class.py
+
+# target = __import__("upload.py")
+
 class TestClass:
     def test_one(self):
         x = "this"
         assert "h" in x
 
-    def test_two(self):
-        x = "hello"
-        assert hasattr(x, "check")
+    # def test_two(self):
+        # print(target)
+        # assert hasattr(x, "check")
+
+    
